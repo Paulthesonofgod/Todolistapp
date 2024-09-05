305d2cd9d77e545ee8c9a9f35eabfe76802682ba
paulthesonofgod <paulajolono.ogar@gmail.com>
2024-09-05 14:33:52 +0100

    Day 13 completed

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 def get_todos(filepath="files/todos.txt"):
+    """Gets todos from a specified file """
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
 def write_todos(todos_arg, filepath="files/todos.txt"):
+    """Writes the output in a text file"""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
 
